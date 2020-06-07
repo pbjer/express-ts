99172a354f99003204595d4975c2c8a9d988e7a2
pbjer <prenticebjerk@gmail.com>
2020-06-06 18:07:16 -0700

    add hot reload

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,11 @@
 import express from "express";
 
 const app = express();
-const port = 8080; // default port to listen
+const port = 3000; // default port to listen
 
 // define a route handler for the default route
 app.get( "/", ( req, res ) => {
-  res.send( "Hello world!" );
+  res.send('200 OK');
 });
 
 // start the Express server
